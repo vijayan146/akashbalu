a4638f1c9f749c56405a99ac126cc098094b27c7
akashbalu <36035645+vijayan146@users.noreply.github.com>
2018-02-08 15:55:09 +0530

    Create ts.c

---
+++ ts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    char c;
+    printf("Enter a character: ");
+    scanf("%c",&c);
+        printf("%c is an alphabet.",c);
+    else
+        printf("%c is not an alphabet.",c);
+
+    return 0;
+}
