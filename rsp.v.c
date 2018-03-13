b7ca51d6d3cc317425bd393a263f08f00880ed6d
akashbalu <36035645+vijayan146@users.noreply.github.com>
2018-03-13 03:22:38 -0700

    Create rsp.v.c

---
+++ rsp.v.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main()
+
+{
+
+    int a,b,num;
+    printf("enter the a,b value");
+    scanf("%d%d",&a,&b);
+    {
+        for(i=a;i<=b,i++)
+        {
+            if(i%2==1)
+            printf("print the i value%d",i);
+        }
+    }
+
+}
