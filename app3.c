297e03ba2e6fe5b60aca056c750a4ad4f1160081
akashbalu <36035645+vijayan146@users.noreply.github.com>
2018-03-16 16:00:28 +0530

    Create app3.c

---
+++ app3.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+int a,b,cnt=0,i,j,c=0;
+printf("Enter the 2 numbers..:");
+scanf("%d%d",&a,&b);
+
+for(i=a;i<b;i++)
+{
+    for(j=2;j<a;j++)
+{
+    if(i%j==0)
+    {
+        cnt=1;
+        break;
+    }
+
+if(cnt==0)
+{
+c=c+1;
+
+}
+}
+printf("prime numbers are:%d",c);
+return 0;
+}
+
+
